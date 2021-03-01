8aad37e78050bad5b95aeabdbf4c82ad11b470de
romadebrian <roma_coll04@yahoo.com>
2021-03-02 01:51:11 +0700

    Update Gradle to 4.0.0 Update SDK to 29 Update to AndroidX

--- app/src/main/java/info/jakelee/spacewars/Spacewar.java
+++ app/src/main/java/info/jakelee/spacewars/Spacewar.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.media.MediaPlayer;
 import android.os.IBinder;
-import android.support.v7.app.AppCompatActivity;
+
+//import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
